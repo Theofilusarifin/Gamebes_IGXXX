d23a646d1a1524b40385851b4279c225bb29998a
Theofilusarifin <theofilusarifin@gmail.com>
2022-07-17 07:14:11 +0700

    Update map.blade.php

--- resources/views/penpos/layouts/map.blade.php
+++ resources/views/penpos/layouts/map.blade.php
@@ -49,6 +49,9 @@
         <nav class="navbar navbar-top navbar-expand navbar-dashboard navbar-dark ps-0 pe-2 pb-0">
             <div class="container-fluid px-0">
                 <div class="d-flex justify-content-between align-items-center w-100 ps-5" id="navbarSupportedContent">
+                    {{-- Card Season --}}
+                    <div class="d-flex align-items-center card border-0 shadow px-3 mt-4">
+                    </div>
                     <!-- Navbar links -->
                     <div class="align-items-center">
                         <ul class="align-items-center navbar-nav w-100">
